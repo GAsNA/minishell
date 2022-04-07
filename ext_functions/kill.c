22fec4f4ab634ead4a868104749620002bdfc7bc
Romane LESEUR <rleseur@e2r2p15.clusters.42paris.fr>
2022-04-07 09:58:01 +0200

    add ext_functions kill, sigaction, signal

---
+++ ext_functions/kill.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+int main(int ac, char **av)
+{
+	int		pid;
+
+	pid = atoi(av[1]);
+	// envoie d'un signal a un processus
+	kill(pid, SIGUSR1); // kill(pid du receveur, type de signal)
+}
---
+++ ext_functions/signal.c
@@ -0,0 +1,10 @@
+static void	handler(int signal) // ce type de prototype
+{
+	// generalement c'est la qu'on 'kill' (envoit de signal)
+}
+
+int main()
+{
+	// initialisation signaux, avec fonction ou sera recu signaux
+	signal(SIGUSR1, handler); // signal(signal, fonction handler)
+}
