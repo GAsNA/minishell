5e661d94362e4d7fab81cff67609d8c159d7aaed
David ASLI <aasli@e2r7p6.clusters.42paris.fr>
2022-04-12 09:53:17 +0200

    Handle signals + norm

--- headers/minishell.h
+++ headers/minishell.h
@@ -1,17 +1,31 @@
-#ifndef		MINISHELL_H
-# define	MINISHELL_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aasli <aasli@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 09:43:07 by aasli             #+#    #+#             */
+/*   Updated: 2022/04/12 09:51:55 by aasli            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
 # include<stdio.h>
 # include<stdlib.h>
+# include<unistd.h>
 # include<readline/readline.h>
 # include<readline/history.h>
+# include<signal.h>
 
-typedef	struct s_data
+typedef struct s_data
 {
 	int		run;
 	int		exit_code;
 	char	**env;
-	char	**line;
+	char	*line;
 }	t_data;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,9 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aasli <aasli@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 09:44:11 by aasli             #+#    #+#             */
+/*   Updated: 2022/04/12 09:51:52 by aasli            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../headers/minishell.h"
+
+void	ctrl_c(int signum)
+{
+	(void)signum;
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void	handle_signals_main(void)
+{
+	signal(SIGINT, ctrl_c);
+	signal(SIGQUIT, SIG_IGN);
+}
 
 int	minishell(char **env)
 {
-	t_data data;
-	
+	t_data	data;
+
+	(void)env;
+	handle_signals_main();
 	data.run = 1;
 	while (data.run)
 	{
@@ -11,18 +40,22 @@ int	minishell(char **env)
 		if (!data.line)
 		{
 			data.run = 0;
-			return (123);
+			printf("exit\n");
+			return (0);
 		}
+		if (data.line[0])
+			add_history(data.line);
+		printf("%s\n", data.line);
 	}
 	return (0);
 }
 
 int	main(int ac, char **av, char **env)
 {
-	(void)ac;
-	(void)av;
 	int	exit_code;
 
+	(void)ac;
+	(void)av;
 	exit_code = minishell(env);
 	return (exit_code);
 }
