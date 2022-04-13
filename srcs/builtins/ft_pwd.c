fe52e741cdba0578185e59d49e6ea0bd0b393687
David ASLI <aasli@e2r7p6.clusters.42paris.fr>
2022-04-13 08:47:44 +0200

    Function pwd + norm

---
+++ srcs/builtins/ft_pwd.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pwd.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aasli <aasli@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 08:44:25 by aasli             #+#    #+#             */
+/*   Updated: 2022/04/13 08:44:29 by aasli            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/minishell.h"
+#include "../../libft/libft.h"
+
+int	ft_pwd(char **cmd)
+{
+	char	*buff;
+
+	if (cmd[1] && cmd[1][0] == '-')
+	{
+		printf("Rovidshell: pwd: %s: options are not handled", cmd[1]);
+		return (1);
+	}
+	buff = calloc(2049, sizeof(char));
+	if (!buff)
+	{
+		printf("Allocation error");
+		return (1);
+	}
+	getcwd(buff, 2048);
+	printf("%s\n", buff);
+	free(buff);
+	return (0);
+}
