ab7af4e221c733372d428c026ad59b8f77eda811
David ASLI <aasli@e2r1p8.clusters.42paris.fr>
2022-04-28 10:28:34 +0200

    linked list env prototype on main branch

--- headers/minishell.h
+++ headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aasli <aasli@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 09:43:07 by aasli             #+#    #+#             */
-/*   Updated: 2022/04/12 10:23:16 by rleseur          ###   ########.fr       */
+/*   Updated: 2022/04/28 10:27:33 by aasli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ enum ops
 
 typedef struct s_data		t_data;
 typedef struct s_command	t_command;
+typedef struct s_lenv		t_lenv;
 
 struct s_data
 {
@@ -45,6 +46,11 @@ struct s_command
 	char		**command;
 	enum ops	op;
 	t_command	*next;
-}
+};
+
+struct s_lenv
+{
+	char		*line;
+};
 
 #endif
