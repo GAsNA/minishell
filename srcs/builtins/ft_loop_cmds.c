a31a1ba0dbed2e27373ed805da61e919eabdd23a
David ASLI <aasli@e2r7p6.clusters.42paris.fr>
2022-05-26 08:27:13 +0200

    Exec with pipe / need an EOF for wc to work, need fix

--- srcs/builtins/ft_loop_cmds.c
+++ srcs/builtins/ft_loop_cmds.c
@@ -22,6 +22,11 @@ void	ft_loop_cmds(t_cmd *cmds, char **env)
 				dup2(tmp->fd_in, 0);
 				close(tmp->fd_in);
 			}
+			else
+			{
+				dup2(tmp->pipe_fd[0], 0);
+				close(tmp->pipe_fd[0]);
+			}
 			if (tmp->next != NULL && tmp->fd_out == 0)
 			{
 				dup2(tmp->pipe_fd[1], 1);
@@ -37,22 +42,20 @@ void	ft_loop_cmds(t_cmd *cmds, char **env)
 		}
 		else if (tmp->pid != 0)
 		{
+			waitpid(tmp->pid, &g_status, 0);
+			close(tmp->pipe_fd[1]);
 			if (tmp->fd_in != 0)
 				close(tmp->fd_in);
+			else
+				close(tmp->pipe_fd[0]);
 			if (tmp->fd_out != 0)
 				close(tmp->fd_out);
-			close(tmp->pipe_fd[0]);
-			close(tmp->pipe_fd[1]);
+			if (tmp->next != NULL )
+			{
+				dup2(tmp->pipe_fd[0], 0);
+				close(tmp->pipe_fd[0]);
+			}
 		}
 		tmp = tmp->next;
-	}
-	tmp = cmds;
-	while (tmp != NULL)
-	{
-		if (tmp-> pid != 0)
-		{
-			waitpid(tmp->pid, &g_status, 0);
-			tmp = tmp->next;
-		}
 	}
 }
