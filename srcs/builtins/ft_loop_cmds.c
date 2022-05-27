eac17a507d4d4576c8ca3e8d7029cbc488b07e37
David ASLI <aasli@e2r7p6.clusters.42paris.fr>
2022-05-27 19:58:54 +0200

    pwd and file descriptor closed in child to work efficiently

--- srcs/builtins/ft_loop_cmds.c
+++ srcs/builtins/ft_loop_cmds.c
@@ -6,7 +6,7 @@
 /*   By: aasli <aasli@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 14:38:45 by aasli             #+#    #+#             */
-/*   Updated: 2022/05/27 16:26:43 by aasli            ###   ########.fr       */
+/*   Updated: 2022/05/27 19:56:15 by aasli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,22 +67,28 @@ int	no_fork_allowed(char **cmd)
 	return (0);
 }
 
-int	launch_builtin(char **cmd, t_lenv **lenv)
+int	launch_builtin(t_cmd *cmd, t_lenv **lenv)
 {
-	if (ft_strncmp(cmd[0], "cd", ft_strlen("cd\0")) == 0)
-		g_status = ft_cd(cmd, lenv);
-	else if (ft_strncmp(cmd[0], "pwd", ft_strlen("pwd\0")) == 0)
-		g_status = ft_pwd(cmd, lenv);
-	else if (ft_strncmp(cmd[0], "env", ft_strlen("env\0")) == 0)
-		g_status = ft_env(cmd, lenv);
-	else if (ft_strncmp(cmd[0], "unset", ft_strlen("unset\0")) == 0)
-		g_status = ft_unset(cmd, lenv);
-	else if (ft_strncmp(cmd[0], "exit", ft_strlen("exit\0")) == 0)
-		g_status = ft_exit(cmd, lenv);
-	else if (ft_strncmp(cmd[0], "export", ft_strlen("export\0")) == 0)
-		g_status = ft_export(cmd, lenv);
-	else if (ft_strncmp(cmd[0], "echo", ft_strlen("echo\0")) == 0)
-		g_status = ft_echo(cmd, lenv);
+	if (ft_strncmp(cmd->cmd[0], "cd", ft_strlen("cd\0")) == 0)
+		g_status = ft_cd(cmd->cmd, lenv);
+	else if (ft_strncmp(cmd->cmd[0], "pwd", ft_strlen("pwd\0")) == 0)
+		g_status = ft_pwd(cmd->cmd, lenv);
+	else if (ft_strncmp(cmd->cmd[0], "env", ft_strlen("env\0")) == 0)
+		g_status = ft_env(cmd->cmd, lenv);
+	else if (ft_strncmp(cmd->cmd[0], "unset", ft_strlen("unset\0")) == 0)
+		g_status = ft_unset(cmd->cmd, lenv);
+	else if (ft_strncmp(cmd->cmd[0], "exit", ft_strlen("exit\0")) == 0)
+		g_status = ft_exit(cmd->cmd, lenv);
+	else if (ft_strncmp(cmd->cmd[0], "export", ft_strlen("export\0")) == 0)
+		g_status = ft_export(cmd->cmd, lenv);
+	else if (ft_strncmp(cmd->cmd[0], "echo", ft_strlen("echo\0")) == 0)
+		g_status = ft_echo(cmd->cmd, lenv);
+	if (cmd->next)
+		close(cmd->pipe_fd[0]);
+	if (cmd->fd_in != -1)
+		close(cmd->fd_in);
+	if (cmd->fd_out != -1)
+		close(cmd->fd_out);
 	return (0);
 }
 
@@ -105,9 +111,9 @@ int	is_builtin(char **cmd)
 	return (0);
 }
 
-int	ft_exec_child(char **cmd, t_lenv **lenv)
+int	ft_exec_child(t_cmd *cmd, t_lenv **lenv)
 {
-	if (is_builtin(cmd))
+	if (is_builtin(cmd->cmd))
 		launch_builtin(cmd, lenv);
 	else
 	{
@@ -118,18 +124,24 @@ int	ft_exec_child(char **cmd, t_lenv **lenv)
 		char *path;
 		if (!env[0])
 			printf("env empty\n");
-		execve(cmd[0], cmd, env);
+		execve(cmd->cmd[0], cmd->cmd, env);
 		while (paths[i])
 		{
 			tmp = ft_strjoin(paths[i], "/");
-			path = ft_strjoin(tmp, cmd[0]);
+			path = ft_strjoin(tmp, cmd->cmd[0]);
 			free(tmp);
-			g_status = execve(path, cmd, env);
+			g_status = execve(path, cmd->cmd, env);
 			free(path);
 			i++;
 		}
 	}
-	return (0);
+	//if (cmd->next == NULL)
+	//{
+		close (0);
+		close (1);
+		close (2);
+//	}
+	exit (0);
 }
 
 void	ft_loop_cmds(t_cmd *cmds, t_lenv **env)
@@ -142,17 +154,14 @@ void	ft_loop_cmds(t_cmd *cmds, t_lenv **env)
 		if (tmp->next)
 			ft_redir_pipe(tmp);
 		if (tmp->next == NULL && no_fork_allowed(tmp->cmd))
-			g_status = launch_builtin(tmp->cmd, env);
+			g_status = launch_builtin(tmp, env);
 		else
 		{
-			ft_putstr_fd("Forking des familles\n", 1);
 			tmp->pid = fork ();
 			if (tmp->pid == 0)
 			{
-				ft_putstr_fd("Child begin\n", 1);
 				ft_prepare_child(tmp);
-				ft_putstr_fd("Child prepared\n", 2);
-				ft_exec_child(tmp->cmd, env);
+				ft_exec_child(tmp, env);
 			}
 			else if (tmp->pid != 0)
 			{
