d72c28cef88c362fde67d9ffc09667c465f2c996
Romane LESEUR <rleseur@e1r6p18.42.fr>
2022-06-06 17:44:56 +0200

    norme ok

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: rleseur <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 10:28:13 by rleseur           #+#    #+#             */
-/*   Updated: 2022/06/06 17:03:42 by rleseur          ###   ########.fr       */
+/*   Updated: 2022/06/06 17:44:27 by rleseur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,47 +35,59 @@ static t_cmd	*get_cmd_and_heredoc(t_regroup *reg)
 	return (cmd);
 }
 
+static t_regroup	*free_and_pass(t_regroup *reg, int redir)
+{
+	if (reg && ft_strcmp(reg->str, "|") != 0)
+	{
+		if (redir)
+			free(reg->str);
+		if (!redir && ft_strcmp(reg->str, "<<") == 0)
+		{
+			free(reg->str);
+			if (reg->next && ft_strcmp(reg->next->str, "|") != 0)
+			{
+				free(reg->next->str);
+				reg = reg->next;
+			}
+		}
+		reg = reg->next;
+	}
+	return (reg);
+}
+
+static t_regroup	*make_redir(t_regroup *reg, int *fd_in, int *fd_out,
+								char ***cmd)
+{
+	int	redir;
+
+	redir = 0;
+	if (ft_strcmp(reg->str, ">") == 0 || ft_strcmp(reg->str, ">>") == 0)
+	{
+		check_fd_out(fd_in, fd_out, reg, cmd);
+		free(reg->str);
+		redir = 1;
+		reg = reg->next;
+	}
+	else if (ft_strcmp(reg->str, "<") == 0)
+	{
+		check_fd_in(fd_in, fd_out, reg, cmd);
+		free(reg->str);
+		redir = 1;
+		reg = reg->next;
+	}
+	reg = free_and_pass(reg, redir);
+	return (reg);
+}
+
 static t_cmd	*get_in_out_file(t_cmd *cmd, t_regroup *reg)
 {
-	int		redir;
 	t_cmd	*rt;
 
 	rt = cmd;
 	while (reg)
 	{
 		while (reg && ft_strcmp(reg->str, "|") != 0)
-		{
-			redir = 0;
-			if (ft_strcmp(reg->str, ">") == 0 || ft_strcmp(reg->str, ">>") == 0)
-			{
-				check_fd_out(&cmd->fd_in, &cmd->fd_out, reg, &cmd->cmd);
-				free(reg->str);
-				redir = 1;
-				reg = reg->next;
-			}
-			else if (ft_strcmp(reg->str, "<") == 0)
-			{
-				check_fd_in(&cmd->fd_in, &cmd->fd_out, reg, &cmd->cmd);
-				free(reg->str);
-				redir = 1;
-				reg = reg->next;
-			}
-			if (reg && ft_strcmp(reg->str, "|") != 0)
-			{
-				if (redir)
-					free(reg->str);
-				if (!redir && ft_strcmp(reg->str, "<<") == 0)
-				{
-					free(reg->str);
-					if (reg->next && ft_strcmp(reg->next->str, "|") != 0)
-					{
-						free(reg->next->str);
-						reg = reg->next;
-					}
-				}
-				reg = reg->next;
-			}
-		}
+			reg = make_redir(reg, &cmd->fd_in, &cmd->fd_out, &cmd->cmd);
 		if (reg)
 		{
 			free(reg->str);
@@ -86,28 +98,21 @@ static t_cmd	*get_in_out_file(t_cmd *cmd, t_regroup *reg)
 	return (rt);
 }
 
-static t_cmd	*get_cmd_prev(t_cmd *cmd)
+t_cmd	*parsing(t_regroup *reg, t_lenv *lenv)
 {
+	t_cmd	*cmd;
 	t_cmd	*tmp;
 
+	if (!reg)
+		return (0);
+	cmd = get_cmd_and_heredoc(reg);
+	cmd = get_in_out_file(cmd, reg);
 	tmp = cmd;
 	while (tmp->next)
 	{
 		tmp->next->prev = tmp;
 		tmp = tmp->next;
 	}
-	return (cmd);
-}
-
-t_cmd	*parsing(t_regroup *reg, t_lenv *lenv)
-{
-	t_cmd		*cmd;
-
-	if (!reg)
-		return (0);
-	cmd = get_cmd_and_heredoc(reg);
-	cmd = get_in_out_file(cmd, reg);
-	cmd = get_cmd_prev(cmd);
 	cmd = get_expands(cmd, lenv);
 	ft_list_clear_reg(reg);
 	return (cmd);
