f6aedd0c58a360d39351f914d0b31f9bc429cb0d
David ASLI <aasli@e2r7p6.42.fr>
2022-06-22 09:24:45 +0200

    refactor unset builtins

--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: aasli <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 10:16:35 by aasli             #+#    #+#             */
-/*   Updated: 2022/06/19 16:04:01 by aasli            ###   ########.fr       */
+/*   Updated: 2022/06/22 09:18:55 by aasli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 #include "../../headers/builtins.h"
 #include "../libft/libft.h"
 
+static int	print_unset_error(char *s1, char *s2, char *s3)
+{
+	char *tmp;
+
+	tmp = ft_strdjoin(s1, s2, s3);
+	if (!tmp)
+		return (1);
+	write(2, tmp, ft_strlen(tmp));
+	free(tmp);
+	return (1);
+}
+
 int	is_bad_identifier(char c)
 {
-	if (isalpha(c) || c == '_')
+	if (isalpha(c) || c == '_' || c == '#')
 		return (0);
 	return (1);
 }
@@ -31,11 +43,8 @@ static int	proceed_unset(char **cmd, t_lenv **env)
 	while (cmd[i])
 	{
 		if (is_bad_identifier(cmd[i][0]) == 1)
-		{
-			printf("Rovidshell: unset: %s: not a valid identifier\n",
-				cmd[i]);
-			status = 1;
-		}
+			status = print_unset_error("Rovidshell: unset: '", cmd[i],
+						"': not a valid identifier\n");
 		else if (check_env_var(env, cmd[i]) == 1)
 			unset_var_env(env, cmd[i], ft_strlen(cmd[i]));
 		i++;
@@ -52,8 +61,9 @@ int	ft_unset(char **cmd, t_lenv **env)
 	{
 		if (cmd[1][0] == '-')
 		{
-			printf("Rovidshell: unset: %s: options are not handled\n", cmd[1]);
-			return (1);
+			print_unset_error("Rovidshell: unset: '", cmd[1],
+						"': options are not handled\n");
+			return (2);
 		}
 		status = proceed_unset(cmd, env);
 	}
